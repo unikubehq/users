3200ed3f5e89c099d5ca639aa2861b1e26e919dc
Robert Stein <SteinRobert@users.noreply.github.com>
2021-10-04 22:21:10 +0200

    fix: add missing query

--- src/gql/schema/__init__.py
+++ src/gql/schema/__init__.py
@@ -3,5 +3,4 @@ from graphene_federation import build_schema
 from .mutation import Mutation
 from .query import Query
 
-schema = build_schema(query=Query)
-# TODO: add Mutation once implemented!
+schema = build_schema(query=Query, mutation=Mutation)
